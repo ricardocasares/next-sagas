83fb6a408ccee4c4ac9f514dee8b5515d51cc801
Ricardo Casares <ricardocasares@gmail.com>
2020-05-07 18:47:46 +0200

    fix: tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   transform: {
-    "^.+\\.tsx?$": "babel-jest"
-  }
+    "^.+\\.tsx?$": "babel-jest",
+    "^.+\\.css$": "<rootDir>/.config/jest/css.js",
+  },
+  moduleNameMapper: {
+    "^.+\\.module\\.(css)$": "identity-obj-proxy",
+  },
 };
